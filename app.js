d85bd1d16fa6bfea83fa1762ea1771c4b17fba97
chandraprakash <chandraprak33@gmail.com>
2020-07-21 02:51:50 +0530

    connected database

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express = require('express');
 
 const app     =  express();
-
+const db  = require('./src/backend/config/mongoose') 
 require('dotenv').config();
 
 const cors    = require('cors'); // express middleware which used to cross-orign requset
 
-const port  = process.env || 3000;
+const port  = process.env.PORT || 5000;
 
 app.use(cors());
 
---
+++ src/backend/config/mongoose.js
@@ -0,0 +1,12 @@
+const  mongoose = require('mongoose');
+//Set up default mongoose connection
+const mongoDB = 'mongodb://localhost:27017/marvel-api';
+mongoose.connect(mongoDB, { useNewUrlParser: true,useUnifiedTopology: true  });
+ //Get the default connection
+var db = mongoose.connection.on('connected', () =>
+ console.log('succesfully connected to database') 
+ );
+//Bind connection to error event (to get notification of connection errors)
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+module.exports  = db;
